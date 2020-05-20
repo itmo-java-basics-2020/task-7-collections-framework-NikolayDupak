7efdbf5599391cb4ee9977cbb5cccaf3219bcaf9
NIkolay <Nikolaydupak@mail.ru>
2020-05-20 12:02:13 +0300

    implement all methods

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,52 @@
 package ru.ifmo.collections;
 
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
+
 public class FirstUnique {
+    HashSet<Integer> hashSet = new HashSet();
+
+    LinkedList<Integer> listQueue = new LinkedList();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+
+        for (int elem = 0; elem < numbers.length; elem++) {
+            this.add(numbers[elem]);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        if(listQueue.isEmpty()){
+            return -1;
+        }
+
+        return listQueue.getFirst();
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (!hashSet.contains(value)) {
+            hashSet.add(value);
+            listQueue.add(value);
+
+        }
+        else {
+            Iterator<Integer> iterator = listQueue.iterator();
+            while(iterator.hasNext())
+            {
+                Integer val = iterator.next();
+                if (val.equals(value))
+                {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
     }
 }
--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,35 @@
 package ru.ifmo.collections;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
+    LinkedList<Integer> listNumbers = new LinkedList();
+    int k;
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        this.k = k - 1;
+        for (int number : numbers) {
+            this.listNumbers.add(number);
+        }
+
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        listNumbers.add(val);
+        return KthElement();
+    }
+
+    private int KthElement() {
+        Collections.sort(listNumbers);
+        return listNumbers.get(this.k);
     }
 }
